2de3e5fb8296e2ad30b1a86e733d0d9f77769c32
Thomasvalenciana <123031751+Thomasvalenciana@users.noreply.github.com>
2024-10-28 20:50:34 -0500

    dl

--- backened/dist/utils/token-manager.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-export const createToken = (id, email, expiresIn) => {
-    const playload = { id, email };
-    const token = jwt.sign(playload, process.env.JWT_SECRET, {
-        expiresIn,
-    });
-    return token;
-};
-//# sourceMappingURL=token-manager.js.map
