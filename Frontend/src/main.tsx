a2c327a57a545d74945f21671aa81840904343d0
Thomasvalenciana <123031751+Thomasvalenciana@users.noreply.github.com>
2024-10-31 16:24:39 -0500

    frontend and backend sync

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -6,6 +6,11 @@ import App from './App.tsx'
 import { createTheme, ThemeProvider } from '@mui/material';
 import{BrowserRouter} from 'react-router-dom';
 import { AuthProvider } from "./context/AuthContext.tsx";
+import axios from "axios";
+import{Toaster} from 'react-hot-toast'
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.withCredentials = true;
+
 
 const theme =createTheme({
   typography: {
@@ -20,6 +25,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
      <ThemeProvider theme ={theme}>
       <App />
      </ThemeProvider>
+    
+     <Toaster position  ="top-right"/>
     </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
