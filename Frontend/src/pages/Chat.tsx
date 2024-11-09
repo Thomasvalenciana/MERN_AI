176482d05a4ba1978c91a990b405db91c219b470
Thomasvalenciana <123031751+Thomasvalenciana@users.noreply.github.com>
2024-11-09 03:02:26 -0600

    chat visual , clear conversation

--- Frontend/src/pages/Chat.tsx
+++ Frontend/src/pages/Chat.tsx
@@ -1,37 +1,63 @@
-
-
-import { Box, Avatar } from "@mui/material";
+import { Box, Avatar , Typography, Button } from "@mui/material";
 import { useAuth } from "../context/AuthContext";
-
+import red from "@mui/material/colors/red";
 
 const Chat = () => {
   const auth = useAuth();
-  return 
- <Box 
-    sx = {{
-    display: "flex", 
-    flex:1,
-    width:"100%",
-    height:"100%",
-    mt:3,
-    gap:3,
 
-    }}>
-      <Box sx = {{ display: {md: "flex", xs: "none", sm: "none"}}}>
-      <Box sx={{diplays:"flex", width: "100%" , height: "60vh", bgcolor: "rgb(17,29,39", boarderRadadius:5, flexDirection:"column", mx:3,}}>
-      <Avatar sx= {{
-      mx: "auto",
-      my:2,
-      bgcolor: "white", 
-      color: "black",
-      fontWeight: 700,
-      }}    
-       >{}</Avatar>
+  return (
+    <Box 
+      sx={{
+        display: "flex", 
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box sx={{ display: { md: "flex", xs: "none", sm: "none" } }}>
+        <Box 
+          sx={{
+            display: "flex", 
+            width: "100%", 
+            height: "60vh", 
+            bgcolor: "rgb(17,29,39)", 
+            borderRadius: 5, 
+            flexDirection: "column", 
+            mx: 3,
+          }}
+        >
+          <Avatar 
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white", 
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0] }{auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+                     You are talking to Unicom the chatBot
+            </Typography>
+            <Typography sx={{ mx: "auto", fontFamily: "work sans", my:4, p:3 }}>
+                     You can ask questions related to knowledge, Buisness, Advices etc..
+            </Typography>
+            <Button sx ={{ width: "200px", my: 'auto', color: 'white', fontWeight: "700", borderRadius:3, mx: "auto" , bgcolor: red[300],
+            ":hover":{
+              bgcolor:red.A400,
+            },
+
+            }}
+            >Clear Conversation
+            </Button>
+        </Box>
       </Box>
-      </Box>
-      </Box>;
-  
+      <Box> </Box>
+    </Box>
+  );
 };
 
-
-export default Chat
+export default Chat;
